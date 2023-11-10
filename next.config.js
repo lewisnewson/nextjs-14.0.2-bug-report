ac1e7db24774a750ac3a1e3174977883e6719f16
Lewis Newson <31809279+lewisnewson@users.noreply.github.com>
2023-11-10 15:19:52 +0000

    Added allowedOrigins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["nextjs-14-0-2-bug-report-4ea064a57303.herokuapp.com"]
+      }
+    }
   }
   
